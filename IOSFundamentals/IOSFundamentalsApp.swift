316d98dde88e96a37807b37268ec7312d4fd7cd6
romanuriel <romanufrancisicomartinez@gmail.com>
2025-01-10 13:27:50 -0600

    Initial Commit

---
+++ IOSFundamentals/IOSFundamentalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSFundamentalsApp.swift
+//  IOSFundamentals
+//
+//  Created by Roman Uriel on 10/01/25.
+//
+
+import SwiftUI
+
+@main
+struct IOSFundamentalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IOSFundamentalsTests/IOSFundamentalsTests.swift
@@ -0,0 +1,17 @@
+//
+//  IOSFundamentalsTests.swift
+//  IOSFundamentalsTests
+//
+//  Created by Roman Uriel on 10/01/25.
+//
+
+import Testing
+@testable import IOSFundamentals
+
+struct IOSFundamentalsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
